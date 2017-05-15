e4c9eb0587777a9cec7b15176151a82c24a7c67f
Abdullah Rahman <abdullah2891@gmail.com>
2017-05-15 21:19:50 +0000

    first commit

---
+++ bookclub/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import BookList
+
+
+class BooklistSerializer(serializers.ModelSerializer): 
+    
+    class Meta:
+        model= BookList
+        fields = ('id', 'name', 'date_created', 'date_modified')
+        read_only_fields = ('date_created', 'date_modified')
+    
+    
+    
+
---
+++ bookclub/api/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework import generics
+from .models import BookList
+from .serializers import BooklistSerializer
+# Create your views here.
+
+class CreateView(generics.ListCreateAPIView): 
+    queryset = BookList.objects.all()
+    serializer_class = BooklistSerializer
+    
+    def perform_create(self, serializer): 
+        serializer.save()
